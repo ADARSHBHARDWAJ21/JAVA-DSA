40d1e82b072ea9a141c37151b018913b480beafd
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-04-03 23:20:02 +0530

    studied array

---
+++ src/Arrays/arrays_04.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class arrays_04 {
+    // Added 'public' and 'String[] args' to make it a valid entry point
+    public static void main(String[] args) {
+        int[] arr = {-6, 8, 14, -2, 23, 47, 4, 3, 10};
+
+        // Initialize max with the first element
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            // If the current element is greater than our current max, update max
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("The maximum element is: " + max);
+    }
+}
+
