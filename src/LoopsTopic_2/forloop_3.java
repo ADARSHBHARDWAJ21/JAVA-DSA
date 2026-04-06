4e8350ed8c4837f59cceb9e878ef08dd7519b89e
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-04-06 23:49:40 +0530

    added

--- src/LoopsTopic_2/forloop_3.java
+++ src/LoopsTopic_2/forloop_3.java
@@ -5,7 +5,7 @@ package LoopsTopic_2;
 public class forloop_3 {
     static void main() {
         for(int i = 73; i<=129; i++){
-            System.out.println(i);
+            System.out.println(i);//
         }
     }
 }
