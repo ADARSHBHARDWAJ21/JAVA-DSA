83ed6620520c71d88704b2ae69d8911b2a8b7885
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-04-25 23:51:50 +0530

    studying two dimensionalarray

---
+++ src/multidimensionalarrays/TwoDimensionalArrays.java
@@ -0,0 +1,25 @@
+package multidimensionalarrays;
+import java.util.Scanner;
+public class TwoDimensionalArrays {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        //arr[0][0] = 9;
+        //input of 2D Array
+        for (int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                arr[i][j] = sc.nextInt();
+            }
+
+        }
+
+
+        //output of 2d Array
+        for (int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/multidimensionalarrays/twodarrayy.java
@@ -0,0 +1,14 @@
+package multidimensionalarrays;
+// 2d array is a grid it is a matrix
+
+public class twodarrayy {
+    static void main(String[] args) {
+    // Representation of 2d Array
+        int[][] grid = new int[3][4];  // 3 row  // 4 column
+        grid[0][0] = 10;
+        grid[0][1] = 20;
+        grid[0][2] = 30;
+        System.out.println(grid[0][2]);
+        System.out.println(grid[0][1]);
+    }
+}
