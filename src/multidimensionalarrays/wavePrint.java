50c470e0ff06eb407dc4c6f79d974d4544ff3879
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-05-03 22:53:15 +0530

    studied arrays

---
+++ src/multidimensionalarrays/wavePrint.java
@@ -0,0 +1,15 @@
+package multidimensionalarrays;
+
+public class wavePrint {
+    public static void print(int[][] arr){}
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int m = arr.length;
+        print(arr);
+    }
+}
