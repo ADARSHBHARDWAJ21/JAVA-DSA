6412485938cc6a58b24353f0289a928bae1804a1
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-05-11 08:46:58 +0530

    studied strings

---
+++ src/Strings/Interningandnewkeyword_10.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Interningandnewkeyword_10 {
+    static void main(String[] args) {
+        String s = "Raghav";
+        s = "Madhav";
+
+        System.out.println(s);
+    }
+}
--- src/Strings/Substring_07.java
+++ src/Strings/Substring_07.java
@@ -7,5 +7,10 @@ public class Substring_07 {
         System.out.println(s.substring(1,4)); // bcd
         // substring(i,j) - this gets part of string from i to j-1
         //substring(i) - it prints i and after i instances
+        //print all substring
+        for (int i = 1; i <=5 ; i++) {
+            System.out.println(s.substring(0,i)+" ");
+
+        }
     }
 }
