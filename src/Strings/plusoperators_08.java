6f9f029b8b0f4af07531ee04e34a07d09c29269d
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-05-13 22:54:44 +0530

    studied strings

--- src/Strings/plusoperators_08.java
+++ src/Strings/plusoperators_08.java
@@ -2,14 +2,14 @@ package Strings;
 
 public class plusoperators_08 {
     static void main(String[] args) {
-        String s = "abc";
+        String s = "abbc";
         String t = "pqr";
 
         s = s += t;
         s = 10+s;
         System.out.println(s);
         System.out.println("Hi i am "+10);
-        System.out.println("abc"+10+20);
-        System.out.println(10+20+"abc");
+        System.out.println("abbc"+10+20);
+        System.out.println(10+20+"abbc");
     }
 }
