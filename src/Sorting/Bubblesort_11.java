573378e09d3c46c94525931a6b96abaff083aae3
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-06-11 23:16:40 +0530

    revising bubble sort again

---
+++ src/Sorting/Bubblesort_11.java
@@ -0,0 +1,23 @@
+package Sorting;
+
+public class Bubblesort_11 {
+    static void main(String[] args) {
+        int [] arr = {5,3,8,4,2};
+        int n = arr.length;
+        //no of passes will alyaws be n-1
+        for (int i = 0; i <n-1 ; i++) {
+            for (int j = 0; j <n-1-i ; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+
+        }
+        for (int i = 0; i <n ; i++) {
+            System.out.println(arr[i] + " ");
+
+        }
+    }
+}
