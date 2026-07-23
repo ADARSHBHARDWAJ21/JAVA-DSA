8f1a751b2c9fb31b6cf1dd4255a8666e1b628d00
ADARSHBHARDWAJ21 <adarshbhardwaj1919@gmail.com>
2026-07-23 20:56:36 +0530

    studied whole list frame work

---
+++ src/Collectionframework/JAVASTACK03.java
@@ -0,0 +1,93 @@
+package Collectionframework;
+
+import java.util.*;
+
+public class JAVASTACK03 {
+    static void main(String[] args) {
+//        Stack<Integer> list = new Stack<>();
+//        // add
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        System.out.println(list);
+//        list.add(40);
+//        System.out.println(list);
+//        list.remove(0);
+//        System.out.println(list);
+//
+//        //addAll
+//        List<Integer> list2 = new Stack<>();
+//        list2.add(101);
+//        list2.add(102);
+//        list2.add(20);
+//
+//        list.addAll(list2);
+//        System.out.println(list);
+//        list.removeAll(list2);
+//        System.out.println(list);
+//
+//        System.out.println(list.size());
+//        System.out.println("Printing list2: "+ list2);
+//        list2.clear();
+//        System.out.println(list2.size());
+//
+//        // I want to traverse list using iterator
+//        Iterator<Integer> iterator = list.iterator();
+//        while(iterator.hasNext()){
+//            System.out.println("Element: " + iterator.next());
+//        }
+//
+//        List<Integer> list3 = new Stack<>();
+//        list3.add(11);
+//        list3.add(12);
+//        list3.add(14);
+//        System.out.println(list3.get(2));
+//        System.out.println("befor ser: " +list3);
+//        list3.set(0,100); // old value at index will be overwrite
+//        System.out.println("after set: " + list3);
+//
+//        list3.set(0,100);
+//        // toArray  // converting any collection to array
+//
+//        Object[]arr = list3.toArray();
+//        for(Object obj: arr){
+//            System.out.println(obj);
+//        }
+//        //contains
+//        System.out.println(list3.contains(100009));
+//
+//        list.add(12);
+//        list.add(6);
+//        System.out.println("Printing Entire List: " +list);
+////
+//        //sort an arrayList'
+//        Collections.sort(list);
+//        System.out.println("Printing Entire List:  "+list);
+//        // HW -> how can we sort nin descending order
+//        //ArrayList<Integer> newList = (ArrayList<Integer>)list.clone();
+//      //  System.out.println("Printing Entire newList:  " + newList);
+//        ArrayList<Integer> marks = new ArrayList<>();
+//        marks.ensureCapacity(100);
+//        System.out.println(marks.isEmpty());
+//       // System.out.println(newList.indexOf(40));
+//
+////
+        Stack<Integer> st =  new Stack<>();
+         st.push(10);
+        System.out.println(st);
+        st.push(11);
+        System.out.println(st);
+        st.push(12);
+        System.out.println(st);
+
+        st.pop();
+        System.out.println(st);
+        System.out.println(st.peek());
+        System.out.println(st.search(12)); // op gives -1 if the element is not there
+        System.out.println(st.empty());
+
+
+
+    }
+
+}
--- src/Collectionframework/Linkedlist_02.java
+++ src/Collectionframework/Linkedlist_02.java
@@ -11,9 +11,36 @@ public class Linkedlist_02 {
         linkedlist.add(14);
         linkedlist.add(23);
         System.out.println(linkedlist.get(2));
-
+         //Linked list as queue and dequeue
         //addFirst and addLast
+        LinkedList<Integer>ll = new LinkedList();
+        ll.add(10);
+        System.out.println(ll);
+        ll.addFirst(1); // use to add element in first
+        System.out.println(ll);
+        ll.addLast(101);  // use to add element in last
+        System.out.println(ll);
 
+        //remove first // remove last
+        ll.removeFirst();
+        System.out.println(ll);
+        ll.removeLast();
+        System.out.println(ll);
+
+        //getFirst() , getLast()
+        System.out.println(ll.getLast());
+        System.out.println(ll.getFirst());
+        // peak // poll  // offer
+        System.out.println(ll.peek()); // prints the 1st no from which linkedlist is starting
+        System.out.println(linkedlist.poll()); // returns the 1st starting element of linked list and removes that element also
+        //After poll
+        System.out.println(linkedlist);
+
+        // offer - adds a specefic element at the end of the linked list
+
+        System.out.println(linkedlist.offer(70));
+        System.out.println(linkedlist);
+        // JAVA VECTOR
 
     }
 }
